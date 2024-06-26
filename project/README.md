7057550f6576290ee123cde8ae0f1abd11e31a86
Thyago Augusto <thyago.abfc@gmail.com>
2024-06-25 21:46:51 -0300

    Início do projeto final

---
+++ project/README.md
@@ -0,0 +1,25 @@
+    # Pokémon - Wordle-like Game
+    #### Video Demo:  <URL HERE>
+    #### Description: Welcome to the Pokémon Wordle-Like Game! This project is a fun and engaging game where players try to guess the Pokémon. The game is inspired by the popular Wordle games and brings a unique twist by incorporating Pokémon.
+
+    Libraries Used
+    - requests: This library is used to interact with the PokéAPI and fetch data about Pokémon.
+    - random: Used to randomly select a Pokémon for the player to guess.
+    - figlet: Adds aesthetic elements to the game by creating stylized text.
+    - inflect: Helps in ensuring that the game's messages and prompts use correct English grammar.
+
+    Example Game Flow
+    Start the Game
+    When the game starts, a random Pokémon is generated, and the player is prompted to start guessing.
+
+    Making a Guess
+    The player makes a guess by typing the name of a Pokémon. The game then checks if the guess is correct.
+
+    Feedback and Hints
+    If the guess is incorrect, the game provides hints to help the player. These hints might include the type of the Pokémon, its generation, or other relevant information.
+
+    Winning the Game
+    If the player guesses the Pokémon correctly, a congratulatory message is displayed, and the player is invited to start a new game.
+
+    Restarting the Game
+    Players can play multiple rounds, with a new random Pokémon generated each time, ensuring that the game remains challenging and fun.
---
+++ project/project.py
@@ -0,0 +1,199 @@
+import requests
+from random import randint
+from pyfiglet import Figlet
+import inflect
+
+p = inflect.engine()
+figlet = Figlet()
+
+class Pokemon:
+    GENERATIONS = {
+        1: [1, 150],
+        2: [151, 251],
+        3: [252, 386],
+        4: [387, 493],
+        5: [494, 649],
+        6: [650, 721],
+        7: [722, 809],
+        8: [810, 905],
+        9: [906, 1025]
+    }  
+
+    def __init__(self, poke_id, name, type1, type2, height, weight):
+        self.poke_id = poke_id
+        self.name = name
+        self.type1 = type1
+        self.type2 = type2
+        self.height = height
+        self.weight = weight
+        
+        for gen in Pokemon.GENERATIONS:
+            if self.poke_id >= Pokemon.GENERATIONS[gen][0] and self.poke_id <= Pokemon.GENERATIONS[gen][1]:
+                self.gen = gen
+                break
+
+    @property
+    def poke_id(self):
+        return self._poke_id
+
+    @poke_id.setter
+    def poke_id(self, poke_id):
+        self._poke_id = poke_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if name != "":
+            self._name = name.capitalize() 
+    
+    @property
+    def type1(self):
+        return self._type1
+
+    @type1.setter
+    def type1(self, type1):
+        if type1 != "":
+            self._type1 = type1.capitalize() 
+
+    @property
+    def type2(self):
+        return self._type2
+
+    @type2.setter
+    def type2(self, type2):
+        if type2 == None:
+            self._type2 = "None"
+        else: 
+            self._type2 = type2.capitalize() 
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+    
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight):
+        self._weight = weight
+
+def main():
+    figlet.setFont(font="big")
+    print(figlet.renderText("POKEDLE"))
+    figlet.setFont(font="drpepper")
+    print(figlet.renderText("A Pokemon Wordle-like"))
+    game()
+
+
+def game():
+    start, ending = select_gen()
+    poke_number = randint(start,ending)
+    poke = get_pokemon(poke_number)
+    i = 0
+    while i < 9:
+        print("\nYou have " + str(9-i) + p.plural(" guess", 9-i))
+        guess = input("Who's that pokémon? ").lower()
+        if guess != poke.name.lower():
+            poke_guess = get_pokemon(guess)
+            if poke_guess != None:
+                if poke.gen > poke_guess.gen:
+                    print("Gen: " + str(poke_guess.gen) + " 🔺")
+                elif poke.gen < poke_guess.gen:
+                    print("Gen: " + str(poke_guess.gen) + " 🔻")
+                else: 
+                    print("Gen: " + str(poke.gen) + " ✅")
+                print("Name: " + poke_guess.name + " ❌")
+                if poke.type1 != poke_guess.type1:
+                    print("Type 1: " + poke_guess.type1 + " ❌")
+                else:
+                    print("Type 1: " + poke_guess.type1 + " ✅")
+                if poke.type2 != poke_guess.type2:
+                    print("Type 2: " + poke_guess.type2 + " ❌")
+                else: 
+                    print("Type 2: " + poke_guess.type2 + " ✅")
+                if poke.height > poke_guess.height:
+                    print("Height: " + str(poke_guess.height) + " 🔺")
+                else:
+                    print("Height: " + str(poke_guess.height) + " 🔻")
+                if poke.weight > poke_guess.weight:
+                    print("Weight: " + str(poke_guess.weight) + " 🔺")
+                else:
+                    print("Weight: " + str(poke_guess.weight) + " 🔻", end="\n\n")
+                i += 1
+        else:
+            print("Pokémon not found, try again.")
+            break
+    if i != 9:
+        print("\nYou won!")
+        print("The secret Pokémon was:")
+        print("Gen: " + str(poke.gen) + " ✅")
+        print("Name: " + poke.name + " ✅")
+        print("Type 1: " + poke.type1 + " ✅")
+        print("Type 2: " + poke.type2 + " ✅")
+        print("Height: " + str(poke.height) + " ✅")
+        print("Weight: " + str(poke.weight) + " ✅")
+    else:
+        print("\nYou lose!")
+        print("The secret Pokémon was:")
+        print("Name: " + poke.name)
+
+def select_gen():
+    while True:
+        try:
+            gen_a = int(input("Select the starting generation: "))
+            if gen_a < 1 or gen_a > 9:
+                print("Invalid generation")
+            else:
+                break
+        except:
+            pass
+    
+    while True:
+        try:
+            gen_b = int(input("Select the ending generation: "))
+            if gen_b < gen_a or gen_a > 9:
+                print("Invalid generation")
+            else:
+                break
+        except:
+            pass
+    
+    start_index = Pokemon.GENERATIONS[gen_a][0]
+    end_index = Pokemon.GENERATIONS[gen_b][1]
+    return start_index, end_index
+
+def get_pokemon(poke):
+    try:
+        response = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(poke))
+    except requests.RequestException:
+        return None
+    
+    r = response.json()
+
+    if len(r["types"]) == 1:
+        poke = Pokemon(r["id"],
+                    r["name"], 
+                    r["types"][0]["type"]["name"],
+                    None,
+                    r["height"],
+                    r["weight"])
+    else:
+        poke = Pokemon(r["id"],
+                    r["name"], 
+                    r["types"][0]["type"]["name"],
+                    r["types"][1]["type"]["name"],
+                    r["height"],
+                    r["weight"])
+    
+    return poke
+
+if __name__ == "__main__":
+    main()
